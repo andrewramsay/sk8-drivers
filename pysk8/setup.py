9c6a4612f5847acb40d652248c4417b47d7aa3ca
Andrew Ramsay <andrew.ramsay@glasgow.ac.uk>
2018-03-09 10:43:38 +0000

    fix dependency, serial => pyserial (!)

--- pysk8/setup.py
+++ pysk8/setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'bgapi', 'serial'
+    'bgapi', 'pyserial'
 ]
 
 # The rest you shouldn't have to touch too much :)
