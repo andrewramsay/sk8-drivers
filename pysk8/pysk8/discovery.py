751933429038af2a143aaf005610ff4036e2ee55
Andrew Ramsay <andrew.ramsay@glasgow.ac.uk>
2018-04-26 22:44:23 +0100

    a few bugfixes/improvements and pretty printing added

--- pysk8/pysk8/discovery.py
+++ pysk8/pysk8/discovery.py
@@ -1,16 +1,24 @@
-import logging
+import struct
 
-import pysk8.util
 from pysk8.util import pp_hex
+from pysk8.constants import *
 
 class Descriptor(object):
     """
     Represents a BLE descriptor within a characteristic
     """
 
-    def __init__(self, handle, uuid):
+    def __init__(self, handle, raw_uuid, uuid):
         self.handle = handle
         self.uuid = uuid
+        self.raw_uuid = raw_uuid
+
+    def pp(self, baseindent=2):
+        indent = '\t' * baseindent
+        print('{}+ Descriptor uuid={}, handle=0x{:04X}'.format(indent, self.uuid, self.handle))
+
+    def __repr__(self):
+        return '[Descriptor] handle=0x{:04X}, uuid={}'.format(self.handle, self.uuid)
 
 class Characteristic(object):
     """
@@ -21,14 +29,14 @@ class Characteristic(object):
         self.handle = handle
         self.value = value
         self.props = value[0]
-        self.char_handle = value[1:3]
+        self.char_handle = struct.unpack('<H', value[1:3])[0]
         self.raw_uuid = value[3:]
         self.uuid = pp_hex(self.raw_uuid)
-        logger.debug('Creating char %04X, %s, %s, %s' % (handle, self.pp_properties(), pp_hex(self.char_handle), self.uuid))
+        # logger.debug('Creating char %04X, %s, %s, %s' % (handle, self.pp_properties(), pp_hex(self.char_handle), self.uuid))
         self.descriptors = {}
 
-    def add_descriptor(self, handle, uuid):
-        self.descriptors[handle] = Descriptor(handle, uuid)
+    def add_descriptor(self, handle, raw_uuid, uuid):
+        self.descriptors[handle] = Descriptor(handle, raw_uuid, uuid)
 
     def can_read(self):
         return (self.props & PROP_READ != 0)
@@ -62,6 +70,22 @@ class Characteristic(object):
 
         return None
 
+    def pp(self, baseindent=1):
+        indent = '\t' * baseindent
+        print('{}Char uuid={}'.format(indent, self.uuid))
+        print('{}\tProperties: {}'.format(indent, self.pp_properties()))
+        print('{}\tHandle: 0x{:04X}'.format(indent, self.handle))
+        print('{}\tChar handle: 0x{:04X}'.format(indent, self.char_handle))
+        print('{}\tNum descs: {}'.format(indent, len(self.descriptors)))
+        for d in self.descriptors.values():
+            d.pp()
+
+    def __repr__(self):
+        s = '[Characteristic] handle={:04X}, uuid={}, descriptors='.format(self.handle, self.uuid) 
+        for d in self.descriptors.values():
+            s += '\n\t{}'.format(d)
+        return s
+
 class Service(object):
     """
     Represents a BLE service 
@@ -76,6 +100,7 @@ class Service(object):
 
     def add_characteristic(self, handle, value):
         self.chars[handle] = Characteristic(handle, value)
+        return self.chars[handle]
 
     def get_characteristic_by_uuid(self, uuid):
         for c in list(self.chars.values()):
@@ -86,4 +111,13 @@ class Service(object):
 
     def get_characteristic_by_handle(self, handle):
         return self.chars.get(handle, None)
+    
+    def pp(self):
+        print('Service uuid={}'.format(self.uuid))
+        print('\tStart handle: 0x{:04X}'.format(self.start_handle))
+        print('\tEnd handle: 0x{:04X}'.format(self.end_handle))
+        print('\tNum chars: {}'.format(len(self.chars)))
+        for c in self.chars.values():
+            c.pp()
+
 
