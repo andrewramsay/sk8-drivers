d52c8f7901e99996fe93901657101b8536af0566
Andrew Ramsay <andrew.ramsay@glasgow.ac.uk>
2018-05-01 14:04:11 +0100

    extend samplerate example

--- pysk8/examples/samplerate.py
+++ pysk8/examples/samplerate.py
@@ -11,8 +11,7 @@ import logging
 
 pysk8.core.logger.setLevel(logging.DEBUG)
 
-
-def samplerate(port, device_name):
+def samplerate(port, device_names):
     with Dongle() as dongle: # this will disconnect any active connections when it goes out of scope
 
         if not dongle.init(port):
@@ -21,7 +20,7 @@ def samplerate(port, device_name):
         # use the scan_and_connect convenience function to scan for a list of
         # one or more devices and then attempt to connect to them if they are
         # detected in the scan results
-        (result, devices) = dongle.scan_and_connect([device_name])
+        (result, devices) = dongle.scan_and_connect(device_names, 5.0)
 
         if not result:
             # scan failed to detect device(s), or connection to those device(s)
@@ -30,41 +29,45 @@ def samplerate(port, device_name):
 
         print('Connections created to {} devices'.format(len(devices)))
 
-        # Each entry in this list is an SK8 object representing the physical device
-        sk8 = devices[0]
-
         # Enable IMU data streaming from the SK8 only (no external IMUs)
-        sk8.enable_imu_streaming([0], enabled_sensors=SENSOR_ALL)
+        for sk8 in devices:
+            sk8.enable_imu_streaming([0, 1, 2, 3, 4], enabled_sensors=SENSOR_ACC|SENSOR_GYRO)
         # start_time = time.time()
         last_time = time.time()
         show_data = False
 
+        logfile = open('log.txt', 'w')
+
         def imu_callback(acc, gyro, mag, index, seq, timestamp, data):
             if show_data:
-                print('[{}] acc={}, mag={}, gyro={}, seq={}'.format(index, acc, mag, gyro, seq))
+                print('{}: [{}] acc={}, mag={}, gyro={}, seq={}'.format(data, index, acc, mag, gyro, seq))
+            logfile.write('{},{},{},{},{}\n'.format(data, index, seq, timestamp, acc))
 
-        sk8.set_imu_callback(imu_callback)
+        for sk8 in devices:
+            sk8.set_imu_callback(imu_callback, sk8.name)
 
         while True:
             try:
                 if time.time() - last_time > 1.0:
 
                     # display the rate at which data packets are being received (from each IMU)
-                    rates = []
-                    drops = []
-                    for i in sk8.get_enabled_imus():
-                        imu = sk8.get_imu(i)
-                        rates.append(imu.get_sample_rate())
-                        drops.append(imu.get_packets_lost())
+                    for sk8 in devices:
+                        rates = []
+                        drops = []
+                        for i in sk8.get_enabled_imus():
+                            imu = sk8.get_imu(i)
+                            rates.append(imu.get_sample_rate())
+                            drops.append(imu.get_packets_lost())
 
-                    if -1 not in rates:
-                        print('------------')
-                        fmt_f = '{:.1f}|' * len(rates)
-                        fmt_d = '{:02d}|' * len(rates)
-                        print('Rates: ' + fmt_f.format(*rates))
-                        print('Drops: ' + fmt_d.format(*drops))
-                    else:
-                        print('Waiting...')
+                        if -1 not in rates:
+                            print('------------')
+                            fmt_f = '{:.1f}|' * len(rates)
+                            fmt_d = '{:02d}|' * len(rates)
+                            print('Rates: ' + fmt_f.format(*rates))
+                            print('Drops: ' + fmt_d.format(*drops))
+                            print(devices[0].get_polling_override())
+                        else:
+                            print('Waiting...')
 
                     last_time = time.time()
 
@@ -74,15 +77,22 @@ def samplerate(port, device_name):
                     ch = getch()
                     if ch == b's':
                         show_data = not show_data
+                    if ch == b'1':
+                        print(devices[0].set_polling_override(50))
+                    if ch == b'0':
+                        print(devices[0].set_polling_override(10))
+
             except KeyboardInterrupt:
                 print('Disconnecting...')
                 break
 
-        sk8.disable_imu_streaming()
-        sk8.disconnect()
+        for sk8 in devices:
+            sk8.disable_imu_streaming()
+            sk8.disconnect()
+        logfile.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         sys.exit('Usage: python samplerate.py <serial port> <device name>')
 
-    samplerate(sys.argv[1], sys.argv[2])
+    samplerate(sys.argv[1], sys.argv[2:])
